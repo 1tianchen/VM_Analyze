cae394a3a492fa09f60cbee7285587c263ae5667
1天辰 <1465669976@qq.com>
2022-10-11 22:40:46 +0800

    添加项目文件。

---
+++ demo/demo.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+//优化关闭
+char buf[1024];
+int main()
+{
+	while (1)
+	{
+		scanf("%s", buf);
+		if (!strcmp(buf, "123"))
+		{
+			printf("ok\n");
+		}
+		else
+		{
+			printf("fali\n");
+		}
+	}
+
+
+}
+
